8dca2f8c049bca2482f83e3a029bfc090c69f97e
ravi <ravi.sharma@javra.com>
2020-01-24 14:48:38 +0545

    add network protocol

--- Calendar/HttpRequest.swift
+++ Calendar/HttpRequest.swift
@@ -1,11 +1,36 @@
 import Foundation
 
+enum RouteError: Error {
+    case InvalidURL
+}
+
 protocol InteractsWithServer {
     
     associatedtype ExpectedResponseModelType: Decodable
     
-    var url: URL { get set }
+    var url: String { get }
+    
+    var apiKey: String { get }
+    
+    func prepareURL() throws -> URL
   
 }
 
+
+extension InteractsWithServer {
+    
+    var url: String {
+        return "https://api.omdb.com"
+    }
+    
+    func prepareUrl() throws -> URL {
+        
+        guard let apiURL = URL(string: self.url) else {
+            throw RouteError.InvalidURL
+        }
+        
+        return apiURL
+    }
+  
+}
 
